94fc19b11ca26896309de585bff63b5deb60b259
irisulaj <sulaj.shaqir1@gmail.com>
2022-03-10 20:39:37 +0100

    first commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,192 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
+    public class NamedObject
+    {
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+
+            get;
+            set;
+        }
+
+
+    }
+
+    public interface IBook
+    {
+
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+
+    public abstract class Book : NamedObject, IBook
+    {
+        public Book(string name) : base(name)
+        {
+        }
+
+        public abstract event GradeAddedDelegate GradeAdded;
+
+        public abstract void AddGrade(double grade);
+
+        public abstract Statistics GetStatistics();
+    }
+
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name)
+        {
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+
+            using (var writer = File.AppendText($"{Name}.txt"))
+            {
+
+                writer.WriteLine(grade);
+
+                if (GradeAdded != null)
+                {
+
+                    GradeAdded(this, new EventArgs());
+                }
+
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+
+                    var number = Double.Parse(line);
+
+                    result.Add(number);
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
+        }
+    }
+
+
+    public class InMemoryBook : Book
+    {
+        public InMemoryBook(string name) : base(name)
+        {
+
+            grades = new List<double>();
+            Name = name;
+        }
+
+
+        public void AddGrade(char letter)
+        {
+
+            switch (letter)
+            {
+
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                case 'D':
+                    AddGrade(60);
+                    break;
+
+
+                case 'E':
+                    AddGrade(50);
+                    break;
+
+                case 'F':
+                    AddGrade(40);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+        public override void AddGrade(double grade)
+        {
+
+            if (grade <= 100.0 && grade >= 0.0)
+            {
+                grades.Add(grade);
+
+                if (GradeAdded != null)
+                {
+
+                    GradeAdded(this, new EventArgs());
+                }
+
+            }
+            else
+            {
+
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+
+            }
+
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+        public override Statistics GetStatistics()
+        {
+
+            var result = new Statistics();
+
+
+            for (var index = 0; index < grades.Count; index++)
+            {
+
+                result.Add(grades[index]);
+            }
+
+
+            return result;
+        }
+        private List<double> grades;
+
+        public const string CATEGORY = "Science";
+        private string name;
+    }
+
+
+
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+namespace GradeBook
+
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var book = new DiskBook("Allen's Grade Book");
+
+            book.GradeAdded += OnGradeAdded;
+
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book named: {book.Name}");
+            Console.WriteLine($"The class average is: {stats.Average:N2}");
+            Console.WriteLine($"The lowest grade is: {stats.Low:N2}");
+            Console.WriteLine($"The highest grade is: {stats.High:N2}");
+            Console.WriteLine($"The letter grade is: {stats.Letter:N2}");
+
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Enter grade or q to quit: ");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+
+                }
+                catch (ArgumentException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+                catch (FormatException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+
+                    Console.WriteLine("Execute code  here");
+                }
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e){
+
+                    Console.WriteLine("A grade was added!!!");
+        }
+        
+    }
+}
